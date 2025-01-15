49dd7c336f643ab549205e6641912f3902ab4dd1
Kate Mackin <katemackin@Kates-MacBook-Pro-2.local>
2025-01-15 16:13:15 -0500

    sign in page html css

--- src/pages/signuppage.jsx
+++ src/pages/signuppage.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import "./homepage.css";
+import "./signuppage.css";
 import Navbar from "../components/navbar";
 import myImage from "../assets/house_for_sale2.jpg";
 
 const Signuppage = () => {
   return (
-    <p>Sign Up Page</p>
-  );
-};
+  <div>
+        <Navbar />
+        <main className="signup-main">
+          <div className="signup-container">
+            <p className="signin-title">Sign In</p>
+            <div className="container">
+              <input type="email" placeholder="Email" className="input" />
+              <input type="password" placeholder="Password" className="input" />
+              <button className="login-button">Login</button>
+              <div className="register-container">
+                <span>Don't have an account?</span>
+                <button className="register-button">Register</button>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  };
 
 export default Signuppage;
